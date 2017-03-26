9ce6cdc6cd5506a0bc0f7db25779ad30193782b6
Zsolt Schutzbach <schutzbach.zsolt@e-vista.hu>
2017-03-26 20:28:28 +0200

    User views

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get  '/regisztracio',  to: 'users#new'
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
